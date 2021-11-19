6b179c60742ca3d2c86b8f4ed3c0084b55ed58c4
Kirby <kirby@jovocop.net>
2021-11-18 22:41:43 -0800

    Setting default env to esp32 and adding a second motion sensor

--- wled00/usermod.cpp
+++ wled00/usermod.cpp
@@ -10,10 +10,21 @@
 
 //Use userVar0 and userVar1 (API calls &U0=,&U1=, uint16_t)
 
+// PIR sensor pin
+const int MOTION_PIN_LEFT = 16; // Left side of bed
+const int MOTION_PIN_RIGHT = 21; // Right side of bed
+ // MQTT topic for sensor values
+const char MQTT_TOPIC_LEFT[] = "/motion-left";
+const char MQTT_TOPIC_RIGHT[] = "/motion-right";
+
+int prevStateLeft = LOW;
+int prevStateRight = LOW;
+
 //gets called once at boot. Do all initialization that doesn't depend on network here
 void userSetup()
 {
-  
+  pinMode(MOTION_PIN_LEFT, INPUT);
+  pinMode(MOTION_PIN_RIGHT, INPUT);
 }
 
 //gets called every time WiFi is (re-)connected. Initialize own network interfaces here
@@ -22,8 +33,48 @@ void userConnected()
 
 }
 
+void publishMqttLeft(String state)
+{
+  //Check if MQTT Connected, otherwise it will crash the 8266
+  if (mqtt != nullptr){
+    char subuf[38];
+    strcpy(subuf, mqttDeviceTopic);
+    strcat(subuf, MQTT_TOPIC_LEFT);
+    mqtt->publish(subuf, 0, true, state.c_str());
+  }
+}
+
+void publishMqttRight(String state)
+{
+  //Check if MQTT Connected, otherwise it will crash the 8266
+  if (mqtt != nullptr){
+    char subuf[38];
+    strcpy(subuf, mqttDeviceTopic);
+    strcat(subuf, MQTT_TOPIC_RIGHT);
+    mqtt->publish(subuf, 0, true, state.c_str());
+  }
+}
+
 //loop. You can use "if (WLED_CONNECTED)" to check for successful connection
 void userLoop()
 {
-  
+  if (digitalRead(MOTION_PIN_LEFT) == HIGH && prevStateLeft == LOW) { // Motion detected
+    publishMqttLeft("ON");
+    prevStateLeft = HIGH;
+  } 
+  if (digitalRead(MOTION_PIN_LEFT) == LOW && prevStateLeft == HIGH) {  // Motion stopped
+    publishMqttLeft("OFF");
+    prevStateLeft = LOW;
+  }
+
+
+  if (digitalRead(MOTION_PIN_RIGHT) == HIGH && prevStateRight == LOW) { // Motion detected
+    publishMqttRight("ON");
+    prevStateRight = HIGH;
+  } 
+  if (digitalRead(MOTION_PIN_RIGHT) == LOW && prevStateRight == HIGH) {  // Motion stopped
+    publishMqttRight("OFF");
+    prevStateRight = LOW;
+  }
 }
+
